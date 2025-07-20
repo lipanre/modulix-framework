431f9449e2ba9d9657a8ea1a65d697e24189a7b3
lipan <lipanre@qq.com>
2025-07-20 21:47:13 +0800

    build: v0.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 allprojects {
     group = "io.github.lipanre"
-    version = "0.0.2"
+    version = "0.0.3"
 
     repositories {
         maven { setUrl("https://maven.aliyun.com/repository/public/") }
