83e69b44a27712585329f34f21e009c6580a331b
lipan <lipanre@qq.com>
2025-07-24 01:22:32 +0800

    feat: 更新版本号

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 allprojects {
     group = "io.github.lipanre"
-    version = "0.0.9"
+    version = "0.0.10"
 
     repositories {
         maven { setUrl("https://maven.aliyun.com/repository/public/") }
