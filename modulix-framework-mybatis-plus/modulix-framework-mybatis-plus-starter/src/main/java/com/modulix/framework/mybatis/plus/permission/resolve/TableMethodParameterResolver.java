f486075cb578f70f2e983bf3be7b0d0e03661087
lipan <lipanre@qq.com>
2025-08-12 07:09:48 +0800

    feat: 数据权限，调整table参数解析器

--- modulix-framework-mybatis-plus/modulix-framework-mybatis-plus-starter/src/main/java/com/modulix/framework/mybatis/plus/permission/resolve/TableMethodParameterResolver.java
+++ modulix-framework-mybatis-plus/modulix-framework-mybatis-plus-starter/src/main/java/com/modulix/framework/mybatis/plus/permission/resolve/TableMethodParameterResolver.java
@@ -3,7 +3,6 @@ package com.modulix.framework.mybatis.plus.permission.resolve;
 import com.modulix.framework.mybatis.plus.permission.DataPermissionParameterResolver;
 import net.sf.jsqlparser.schema.Table;
 import org.springframework.core.MethodParameter;
-import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 
@@ -13,7 +12,6 @@ import java.util.Objects;
  * <br>
  * {@code date} 2025/3/4 14:52
  */
-@Component
 public class TableMethodParameterResolver implements DataPermissionParameterResolver {
 
     @Override
